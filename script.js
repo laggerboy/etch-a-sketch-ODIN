8034791a3cc98ceeafa67321fa49d2d4a37b9a45
laggerboy <labibjbaee@gmail.com>
2024-10-10 22:01:04 +0300

    created the grid

---
+++ script.js
@@ -0,0 +1,11 @@
+const contianer = document.querySelector(".container");
+const divs = [];
+
+for (let i = 0; i < 16; i++) {
+  divs[i] = document.createElement("div");
+  contianer.appendChild(divs[i]);
+}
+
+contianer.appendChild(divs[i]);
+console.log("penis");
+aler("penis");
