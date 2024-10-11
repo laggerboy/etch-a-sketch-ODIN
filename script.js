373c44fb64ce0dcce4ce81558d7bf486acf5c43b
laggerboy <labibjbaee@gmail.com>
2024-10-12 00:29:08 +0300

    fresh start

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const contianer = document.querySelector(".container");
 const divs = [];
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   divs[i] = document.createElement("div");
   contianer.appendChild(divs[i]);
 }
-
-contianer.appendChild(divs[i]);
-console.log("penis");
-aler("penis");
