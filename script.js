75e483d723e9fbb623bdcaf8891a3ce079703afc
laggerboy <labibjbaee@gmail.com>
2024-10-12 00:29:27 +0300

    fresh start

--- script.js
+++
@@ -1,7 +0,0 @@
-const contianer = document.querySelector(".container");
-const divs = [];
-
-for (let i = 0; i < 256; i++) {
-  divs[i] = document.createElement("div");
-  contianer.appendChild(divs[i]);
-}
