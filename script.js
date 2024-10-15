e05cd4cdcd502eefeb7c8ff608394fcf925bc37e
laggerboy <labibjbaee@gmail.com>
2024-10-15 10:42:18 +0300

    all buttons work, project finished.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let gridSize = 16;
-let opacity = 0.02;
+let color = "black";
+let isToggled = false;
+
 // create div to fill the grid
 let div = document.createElement("div");
+
 const gridBtn = document.querySelector("#grid-btn");
 const resetBtn = document.querySelector("#reset-btn");
 const colorBtn = document.querySelector("#color-btn");
@@ -18,9 +21,19 @@ for (let i = 0; i < gridSize * gridSize; i++) {
 container.addEventListener("mouseover", function () {
   // prevent event from firing on parent div
   if (event.target !== event.currentTarget) {
-    event.target.style.backgroundColor = ` black`;
-    // event.target.style.opacity = opacity;
-    // opacity += 0.02;
+    if (colorBtn.className === "active") {
+      event.target.style.backgroundColor = `rgb( ${rgb()}, ${rgb()}, ${rgb()})`;
+    } else if (darkenBtn.className === "active") {
+      if (event.target.style.backgroundColor === "aliceblue") {
+        event.target.style.opacity = 0.1;
+        event.target.style.backgroundColor = "blue";
+      } else {
+        event.target.style.opacity =
+          parseFloat(event.target.style.opacity) + 0.1;
+      }
+    } else {
+      event.target.style.backgroundColor = "black";
+    }
   }
 });
 
@@ -40,12 +53,33 @@ gridBtn.addEventListener("click", function () {
 });
 
 resetBtn.addEventListener("click", function () {
+  reset();
+});
+
+colorBtn.addEventListener("click", function () {
+  colorBtn.classList.toggle("active");
+  darkenBtn.classList.remove("active");
+});
+
+darkenBtn.addEventListener("click", function () {
   Array.from(container.children).forEach((child) => {
     child.style.backgroundColor = "aliceblue";
+    child.style.opacity = 1;
   });
+  darkenBtn.classList.toggle("active");
+  colorBtn.classList.remove("active");
 });
 
 function rgb() {
   return Math.floor(Math.random() * 256);
 }
-// rgb( ${rgb()}, ${rgb()}, ${rgb()})
+
+function reset() {
+  Array.from(container.children).forEach((child) => {
+    child.style.backgroundColor = "aliceblue";
+    child.style.opacity = 1;
+  });
+  color = "black";
+  darkenBtn.classList.remove("active");
+  colorBtn.classList.remove("active");
+}
